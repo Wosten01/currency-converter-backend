ce35ddd31e7e970cd038d20d0b4ea56fb9ff185c
Vladislav Kostenko <mribloko@gmail.com>
2024-09-29 17:38:11 +0400

    improves

---
+++ src/currency/currency.controller.ts
@@ -0,0 +1,60 @@
+import { BadRequestException, Controller, Get, Param } from '@nestjs/common';
+import { CurrencyService } from './currency.service';
+
+@Controller('currency')
+export class CurrencyController {
+  constructor(private readonly currencyService: CurrencyService) {}
+
+  @Get(':code')
+  findByCode(@Param('code') code: string) {
+    if (code.length != 3) {
+      throw new BadRequestException({
+        statusCode: 400,
+        message: `Unknown code format received`,
+        error: 'Bad Request',
+      });
+    }
+
+    const currency = this.currencyService.findByCode(code.toUpperCase());
+
+    const currentDate = new Date().toISOString();
+
+    if (!currency) {
+      throw new BadRequestException({
+        result: 'error',
+        statusCode: 400,
+        message: `Currency with code ${code} not found`,
+        error: 'Bad Request',
+      });
+    }
+
+    return {
+      base_currency: code,
+      rates: currency,
+      date: currentDate,
+    };
+  }
+
+  @Get(':from/to/:to')
+  getExchangeRate(@Param('from') from: string, @Param('to') to: string) {
+    const fromUpper = from.toUpperCase();
+    const toUpper = to.toUpperCase();
+
+    const amount = this.currencyService.getExchangeRate(fromUpper, toUpper);
+    const currentDate = new Date().toISOString();
+
+    if (amount === null) {
+      throw new BadRequestException({
+        statusCode: 400,
+        message: `Exchange rate from ${fromUpper} to ${toUpper} not found`,
+        error: 'Bad Request',
+      });
+    }
+    return {
+      base_currency: fromUpper,
+      target_currency: toUpper,
+      amount: amount,
+      date: currentDate,
+    };
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Currency API')
+    .setDescription('The API for getting data on current exchange rates.')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
